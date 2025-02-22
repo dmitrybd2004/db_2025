cbdc6e8992b96938da70ed0664245a352d7d5774
Pepega00 <gshatrk@gmail.com>
2025-02-22 14:18:57 +0300

    added sign in, failed login loops back to login page

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <?php
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if(isset($_POST['login_btn'])){
         //retrieve form data
-        $username = $_POST['username'];
+        $username = $_POST['username']; 
         $password = $_POST['password'];
 
         //Database connection
@@ -16,13 +16,12 @@
         $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
 
         if($conn->connect_error){
-            die("Connection failed ". $conn->connect_error);
+            die("Connetion failed: ". $conn->connect_error);
+
         }
 
-
-        //validate Login
-        $query = "SELECT * FROM login WHERE username='$username' AND password='$password'";
-
+        // validate login authentication
+        $query = "SELECT * FROM login WHERE login.username='$username' AND login.password='$password' ";
         $result = $conn->query($query);
 
         if($result->num_rows == 1){
@@ -32,12 +31,58 @@
         }
         else{
             //Login failed
-
-            header("Location: error.html");
+            header("Location: index.html");
             exit();
         }
 
         $conn->close();
     }
+    else if(isset($_POST['sign_in_btn'])){
+        //retrieve form data
+        $username = $_POST['username']; 
+        $password = $_POST['password'];
 
+        //Database connection
+
+        $host = "localhost";
+        $dbusername = "root";
+        $dbpassword = "3215979361";
+        $dbname = "auth";
+
+        $conn = new mysqli($host,$dbusername,$dbpassword,$dbname);
+
+        if($conn->connect_error){
+            die("Connetion failed: ". $conn->connect_error);
+
+        }
+
+        // validate login authentication
+        $namequery = "SELECT * FROM login WHERE login.username='$username'";
+        $passquery = "SELECT * FROM login WHERE login.password='$password' ";
+        $nameresult = $conn->query($namequery);
+        $passresult = $conn->query($passquery);
+
+        if($nameresult->num_rows > 0){
+            // Name is taken
+            //$Sign_in_error = "username already taken";
+            echo $Sign_in_error;
+            header("Location: index.html");
+            exit();
+        }
+        else if($passresult->num_rows > 0){
+            // Password is taken
+            header("Location: index.html");
+            exit();
+        }
+        else{
+            $insertquery = "INSERT INTO login (username, password) VALUES('$username', '$password')";
+            // $insertquery = "INSERT INTO login (username, password) VALUES($username, $password)";
+            $conn->query($insertquery);
+            header("Location: success.html");
+            exit();
+        }
+
+        $conn->close();
+    }
+}
 ?>
